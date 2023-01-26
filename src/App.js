263d0bb2f47a24290c1ec674e647ecbeea27a8d0
NicholasLe04 <Nicholasl.le224@gmail.com>
2023-01-25 22:56:38 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CourseDropdown from './components/CourseDropdown';
+
+function App() {
+  return (
+    <>
+      <CourseDropdown />
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/CourseDropdown.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import '../App.css'
+import Select from 'react-select';
+
+const courses = [
+  {label: 'CS 46A - Introduction to Programming', value: 'CS 46A'},
+  {label: 'CS 46B', value: 'CS 46B'},
+  {label: 'CS 47', value: 'CS 47'},
+  {label: 'CS 146', value: 'CS 146'},
+  {label: 'CS 155', value: 'CS 155'},
+]
+
+var completed_courses
+
+
+
+export default function CourseDropdown() {
+
+  return (
+    <Select
+      options={courses}
+      isMulti
+      onChange={opt => console.log(opt)}
+    />
+  )
+}
