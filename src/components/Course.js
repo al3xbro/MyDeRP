815634b049a062efdb1b57d692189184aada9c66
NicholasLe04 <Nicholasl.le224@gmail.com>
2023-01-26 14:26:44 -0800

    react collapsible component

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,13 +1,15 @@
 import React from 'react'
+import Collapsible from 'react-collapsible';
 
 function Course({ code, title, desc, prereqs, reqs }) {
   return (
     <div className='Course-tile'>
-      <h2>{code}</h2>
-      <h3>{title}</h3>
-      <p>{desc}</p>
-      <p><h4>Prerequisites: </h4>{prereqs}</p>
-      <p><h4>Requirements filled: </h4>{reqs}</p>
+      <Collapsible trigger={code} transitionTime='100'>
+        <h3>{title}</h3>
+        <p>{desc}</p>
+        <p><h4>Prerequisites: </h4>{prereqs}</p>
+        <p><h4>Requirements filled: </h4>{reqs}</p>
+      </Collapsible>  
     </div>
   )
 }
