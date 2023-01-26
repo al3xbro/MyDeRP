d40c16e7e82649716039d4aca5d1ba2a9e35f580
al3xbro <alexnbui@gmail.com>
2023-01-26 15:53:22 -0800

    Added box borders and fixed all CSS

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <>
       <div className="flex-container">
         <div className="addExCredit">
+          <div className="listName">Add Existing Credit</div>
           <ul>
             <li><Course code={"CS 46A"} title={"intro to nba"} desc={"learn to dunk"} prereqs={"get drafted"} reqs={"high school basketball"} /></li>
             <li><Course code={"CS 46B"} title={"intro to nba"} desc={"learn to dunk"} prereqs={"get drafted"} reqs={"high school basketball"} /></li>
@@ -19,9 +20,11 @@ function App() {
           </ul>
         </div>
         <div className="exCredit">
+          <div className="listName">Current Credits</div>
           <CourseDropdown />
         </div>
         <div className="req">
+          <div className="listName">Graduation Requirements</div>
           <CourseDropdown />
         </div>
       </div>
--- src/components/Course.js
+++ src/components/Course.js
@@ -3,12 +3,12 @@ import Collapsible from 'react-collapsible';
 
 function Course({ code, title, desc, prereqs, reqs }) {
   return (
-      <Collapsible trigger=<h1>{code}</h1> transitionTime='100'>
-        <h3>{title}</h3>
-        <p>{desc}</p>
-        <p><h4>Prerequisites: </h4>{prereqs}</p>
-        <p><h4>Requirements filled: </h4>{reqs}</p>
-      </Collapsible>  
+    <Collapsible trigger=<div className="courseCode">{code}</div> transitionTime='100'>
+      <h3>{title}</h3>
+      <p>{desc}</p>
+      <p><h4>Prerequisites: </h4>{prereqs}</p>
+      <p><h4>Requirements filled: </h4>{reqs}</p>
+    </Collapsible>
   );
 };
 
