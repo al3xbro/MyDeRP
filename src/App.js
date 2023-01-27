4949c307995fcddcdc1514b6a3739f34cffba4cb
Nicholas Le <nicholasl.le224@gmail.com>
2023-01-26 21:46:45 -0800

    Fixed course list overflow + css touchup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
-import CourseDropdown from './components/CourseDropdown';
 import Course from './components/Course';
+
+var completed_courses = [];
 
 function App() {
   return (
@@ -9,23 +10,19 @@ function App() {
         <div className="addExCredit">
           <div className="listName">Add Existing Credit</div>
           <ul>
-            <li><Course code={"CS 46A"} title={"intro to nba"} desc={"learn to dunk"} prereqs={"get drafted"} reqs={"high school basketball"} /></li>
-            <li><Course code={"CS 46B"} title={"intro to nba"} desc={"learn to dunk"} prereqs={"get drafted"} reqs={"high school basketball"} /></li>
-            <li><Course code={"CS 46A"} title={"intro to nba"} desc={"learn to dunk"} prereqs={"get drafted"} reqs={"high school basketball"} /></li>
-            <li><Course code={"CS 46B"} title={"intro to nba"} desc={"learn to dunk"} prereqs={"get drafted"} reqs={"high school basketball"} /></li>
-            <li><Course code={"CS 46A"} title={"intro to nba"} desc={"learn to dunk"} prereqs={"get drafted"} reqs={"high school basketball"} /></li>
-            <li><Course code={"CS 46B"} title={"intro to nba"} desc={"learn to dunk"} prereqs={"get drafted"} reqs={"high school basketball"} /></li>
-            <li><Course code={"CS 46A"} title={"intro to nba"} desc={"learn to dunk"} prereqs={"get drafted"} reqs={"high school basketball"} /></li>
-            <li><Course code={"CS 46B"} title={"intro to nba"} desc={"learn to dunk"} prereqs={"get drafted"} reqs={"high school basketball"} /></li>
+            <li><Course code={"CS 46A"} title={"Introduction to Programming"} units="4" desc={"Basic skills and concepts of computer programming in an object-oriented approach using Java. Classes, methods and argument passing, control structures, iteration. Basic graphical user interface programming. Problem solving, class discovery and stepwise refinement. Programming and documentation style. Weekly hands-on activity."} prereqs={"Math Enrollment Category M-I, M-II, or M-III, or MATH 1"} reqs={"Major Requirement"} /></li>
+            <li><Course code={"CS 46B"} title={"Introduction to Data Structures"} units="4" desc={"Stacks and queues, recursion, lists, dynamic arrays, binary search trees. Iteration over collections. Hashing. Searching, elementary sorting. Big-O notation. Standard collection classes. Weekly hands-on activity."} prereqs={"CS 46A or CS 49J. Math Enrollment Category M-I or M-II and Precalculus Proficiency Assessment (70 or higher), or MATH 19, or MATH 18A and MATH 18B."} reqs={"Major Requirement"} /></li>
+            <li><Course code={"CS 47"} title={"Introduction to Computer Systems"} units="3" desc={"Instruction sets, assembly language and assemblers, linkers and loaders, data representation and manipulation, interrupts, pointers, function calls, argument passing, and basic gate-level digital logic design."} prereqs = {"CS 42 / MATH 42 or CS 42X / MATH 42X, and CS 46B"} reqs={"Major Requirement"} /></li>
+            <li><Course code={"CS 146"} title={"Data Structures and Algorithms"} units="3" desc={"Implementations of advanced tree structures, priority queues, heaps, directed and undirected graphs. Advanced searching and sorting (radix sort, heapsort, mergesort, and quicksort). Design and analysis of data structures and algorithms. Divide-and-conquer, greedy, and dynamic programming algorithm design techniques."} prereqs={"MATH 30, MATH 42, CS 49J, and CS 46B"} reqs={"Major Requirement"}/></li>
           </ul>
         </div>
         <div className="exCredit">
           <div className="listName">Current Credits</div>
-          <CourseDropdown />
+
         </div>
         <div className="req">
           <div className="listName">Graduation Requirements</div>
-          <CourseDropdown />
+
         </div>
       </div>
     </>
--- src/components/Course.js
+++ src/components/Course.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Collapsible from 'react-collapsible';
+import './components.css'
 
-function Course({ code, title, desc, prereqs, reqs }) {
+function Course({ code, title, units, desc, prereqs, reqs }) {
   return (
-    <Collapsible trigger=<div className="courseCode">{code}</div> transitionTime='300' easing="ease">
-      <h3>{title}</h3>
+    <Collapsible trigger=<div className="courseCode">{code}</div> transitionTime='200' easing="ease">
+      <p><h3>{title}</h3> {units} unit(s)</p>
       <p>{desc}</p>
       <p><h4>Prerequisites: </h4>{prereqs}</p>
       <p><h4>Requirements filled: </h4>{reqs}</p>
